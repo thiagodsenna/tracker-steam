1fc178752346a6c1e9a8d17d2880891298acbd29
Thiago Senna <142407130+thiagodsenna@users.noreply.github.com>
2026-07-13 19:03:35 -0300

    Added feedly-proxy
    Added feedly-proxy

---
+++ api/feedly-proxy.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  // URL que você estava usando antes
+  const url = 'https://api.feedly.com/v3/streams/contents?streamId=feed%2Fhttps%3A%2F%2Fwww.skidrowreloaded.com%2Fcategory%2Fpc-games%2Ffeed%2F&count=200&ranked=newest&similar=true&ct=feedly.desktop&cv=31.0.3081';
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    // Libera o CORS para o seu site
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ error: 'Falha ao buscar feedly' });
+  }
+}
