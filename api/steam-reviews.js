3906ce7b2ce9aa867b54f5e44e9f7afb694264df
Thiago Senna <thiago_epd@hotmail.com>
2026-07-15 00:58:46 -0300

    Ajuste filtro api

--- api/steam-reviews.js
+++ api/steam-reviews.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   try {
     // Busca os reviews utilizando os parâmetros para retornar todos desde o início (day_range=all),
     // em todos os idiomas (language=all) e ordenados pelos mais úteis (filter=all)
-    const response = await fetch(`https://store.steampowered.com/appreviews/${appid}?json=1&filter=all&language=all&day_range=90&num_per_page=20`);
+    const response = await fetch(`https://store.steampowered.com/appreviews/${appid}?json=1&filter=all&language=all&day_range=360&num_per_page=20`);
     const data = await response.json();
     
     return res.status(200).json(data);
