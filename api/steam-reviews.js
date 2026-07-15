dacd4a628a86bb9c16cc896844f757ef148b2d84
Thiago Senna <thiago_epd@hotmail.com>
2026-07-15 01:07:14 -0300

    Filtra idiomas preferidos nos reviews

--- api/steam-reviews.js
+++ api/steam-reviews.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   try {
     // Busca os reviews utilizando os parâmetros para retornar todos desde o início (day_range=all),
     // em todos os idiomas (language=all) e ordenados pelos mais úteis (filter=all)
-    const response = await fetch(`https://store.steampowered.com/appreviews/${appid}?json=1&filter=all&language=all&day_range=all&num_per_page=20`);
+    const response = await fetch(`https://store.steampowered.com/appreviews/${appid}?json=1&filter=all&language=all&day_range=1000&num_per_page=40`);
     const data = await response.json();
     
     return res.status(200).json(data);
