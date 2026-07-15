d846c18ce17fe8ae782810ed904527ad2a5ba932
Thiago Senna <thiago_epd@hotmail.com>
2026-07-14 22:59:15 -0300

    Ajustes no review

--- api/steam-reviews.js
+++ api/steam-reviews.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
 
   try {
     // Busca os reviews (por padrão vem os mais recentes/relevantes)
-    const response = await fetch(`https://store.steampowered.com/appreviews/${appid}?json=1&language=brazilian&num_per_page=5`);
+    const response = await fetch(`https://store.steampowered.com/appreviews/${appid}?json=1&language=brazilian&num_per_page=20`);
     const data = await response.json();
     
     return res.status(200).json(data);
