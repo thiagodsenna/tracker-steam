17ca2e197e0f50baa30460433447aab3b8675aed
Thiago Senna <thiago_epd@hotmail.com>
2026-07-28 01:45:10 -0300

    Debug torbox

--- api/torbox-proxy.js
+++ api/torbox-proxy.js
@@ -162,13 +162,7 @@ export default async function handler(req, res) {
 
             await Promise.all(promises);
 
-            return res.status(200).json({
-                items: validItems,
-                debug_raw: {
-                    acao: "web-download",
-                    detalhesPorLink: debugList
-                }
-            });
+            return res.status(200).json({ items: validItems });
         }
 
         // ------------------------------------------------------------------------
